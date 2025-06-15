da87c40caeb43e06c6bcbed1e7cadb45f9f36bfc
Erinle Samuel <psalmuelle1@gmail.com>
2025-06-15 20:43:28 +0100

    feat: contact section

--- src/components/home/ContactSection.tsx
+++ src/components/home/ContactSection.tsx
@@ -6,8 +6,18 @@ export default function ContactSection() {
       <Section
         title="Contact"
         children={
-          <div className="mt-6 md:mt-8">
-            <div></div>
+          <div className="mt-6 text-lg md:mt-8">
+            <div>
+              I would love to hear from you! Whether you have a question, or
+              want to collaborate, feel free to reach out.
+              <br />
+              <a
+                href="mailto:psalmuelle1@gmail.com"
+                className="font-bold text-green-900 hover:underline"
+              >
+                psalmuelle1@gmail.com
+              </a>
+            </div>
           </div>
         }
       />
